dbd7bfedb39b4c2477b10183a1f2cbb3a416c679
UserBraian <113941279+UserBraian@users.noreply.github.com>
2022-11-24 23:18:58 +0100

    Comentarios métodos updateTree, start y stop Task

--- src/main/Task.java
+++ src/main/Task.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /*
-* La clase Task hereda de main.Component
+* La clase Task hereda de Component. Esta clase almacena todos lo intérvalos que se han realizado para
+* completar dicha tarea en un ArrayList<>. Sus funciones principales son empezar una tarea (startTask),
+* para una tarea (stopTask) y por último actualizar el arbol de tareas (updateTree), pasando los datos 
+* hacia la jerarquía superior, en este caso a su padre Project.
 * */
 
 public class Task extends Component {
@@ -33,6 +36,11 @@ public class Task extends Component {
     this.intervals = intervals;
   }
 
+  /*
+  * El método startTask() antes de poder iniciar una tarea, se comprueba que no haya ningún intérvalo
+  * en curso, o por otro lado que el ArrayList de intervalos esté vacio y este sea el primero.
+  * Una vez hechas las comprobaciones se crea un nuevo intérvalo.
+  */
   public void startTask() {
     if (intervals.isEmpty() || intervals.get(intervals.size() - 1).hasEnded()) {
       Interval i = new Interval(this);
@@ -45,6 +53,10 @@ public class Task extends Component {
 
   }
 
+  /*
+  * El método stopTask() para esta tarea llamando al último intérvalo de su lista, y a su vez
+  * detiene dicho intérvalo.
+  */
   public void stopTask() {
     int last = intervals.size() - 1;
     Interval i = intervals.get(last);
@@ -52,7 +64,12 @@ public class Task extends Component {
     //System.out.println("Paramos tarea: " + this.getName());
     logger.info("Paramos tarea: " + this.getName());
   }
-
+  
+  /* 
+  * El método updateTree() forma parte de sistema para actualizar las fechas y la duración 
+  * y una vez acaba se ejecuta la misma función pero en el parent. Se le pasan las variables
+  * de inicio y final para que el proyecto padre recalcule las duraciones y la fecha final.
+  */
   public void updateTree(LocalDateTime start, LocalDateTime end) {
     //cogemos el primero porque es la primera fecha, aunque luego haya más intervalos
     //comprobamos que no esté inicializado
