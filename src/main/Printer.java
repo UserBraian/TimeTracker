a9bd398b348aaa6a3affccd1a944b63306f3bb1f
HectorBenitoPastor <65306454+HectorBenitoPastor@users.noreply.github.com>
2022-11-24 22:56:05 +0100

    Update Printer.java
    coments

--- src/main/Printer.java
+++ src/main/Printer.java
@@ -19,6 +19,7 @@ public class Printer implements Visitor {
   }
 
   /*---- MÉTODOS ----*/
+  //Usando el patron singleton creamos un solo objeto printer
   public static Printer getInstance() {
     if (instance == null) {
       instance = new Printer();
@@ -26,10 +27,17 @@ public class Printer implements Visitor {
     return instance;
   }
 
+  //llamamos al visitor para empezar a printar desde el intervalo activo
   public void print(Interval interval) {
     interval.acceptVisitor(this);
   }
 
+  /*
+  * Funciones del patron visitor, modificadas en printer para que impriman los nombres,
+  * fecha de inicio y fecha final de cada proyecto/task/interval por el que pasen, y 
+  * además redirigen el printer a su padre, y en caso de que ya se haya llegado al 
+  * root entonces paran
+  */
   @Override
   public void visitProject(Project project, Component parent) {
     System.out.println("main.Project:  " + project.getName() + "\t" + project.getStartDate() + "\t"
