4e99e8d1c228919c494d153386371792f461a1be
UserBraian <113941279+UserBraian@users.noreply.github.com>
2022-11-25 12:41:25 +0100

    Delete Visitor.java

--- src/Visitor.java
+++
@@ -1,7 +0,0 @@
-import java.awt.color.CMMException;
-
-public interface Visitor {
-  void visitProject(Project project, Component parent);
-  void visitTask(Task task, Component parent);
-  void visitInterval(Interval interval, Component parent );
-}
